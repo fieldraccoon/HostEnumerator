d32d508e1a57e775e49cc66930bb31c1d6bc3f6c
fieldraccoon <66012425+fieldraccoon@users.noreply.github.com>
2020-10-26 16:01:03 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install gobuster wfuzz ffuf nmap nikto
+sudo apt install gobuster wfuzz ffuf nmap nikto smbmap
 cd /opt 
 git clone https://github.com/ernw/nmap-parse-output.git
 chmod +x /opt/nmap-parse-output/nmap-parse-output
